2bc6fa2b336e20dadf217e440d51ed4b7d87fac9
Voytek80 <50530882+Voytek80@users.noreply.github.com>
2023-06-02 13:18:18 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+
+db = client["example_database2"]
+
+customers = db["customers"]
+items = db["items"]
+
+customers_data = [{ "firstname": "Bob", "lastname": "Adams" },
+                  { "firstname": "Amy", "lastname": "Smith" },
+                  { "firstname": "Rob", "lastname": "Bennet" },]
+items_data = [{ "title": "USB", "price": 10.2 },
+              { "title": "Mouse", "price": 12.23 },
+              { "title": "Monitor", "price": 199.99 },]
+
+customers.insert_many(customers_data)
+items.insert_many(items_data)
+
+bob = customers.update_many(
+        {"firstname": "Bob"},
+        {
+            "$set": {
+                "boughtitems": [
+                    {
+                        "title": "USB",
+                        "price": 10.2,
+                        "currency": "EUR",
+                        "notes": "Customer wants it delivered via FedEx",
+                        "original_item_id": 1
+                    }
+                ]
+            },
+        }
+    )
+
+amy = customers.update_many(
+        {"firstname": "Amy"},
+        {
+            "$set": {
+                "boughtitems":[
+                    {
+                        "title": "Monitor",
+                        "price": 199.99,
+                        "original_item_id": 3,
+                        "discounted": False
+                    }
+                ]
+            } ,
+        }
+    )
+print(type(amy))
+customers.create_index([("name", pymongo.DESCENDING)])
+items = customers.find().sort("name", pymongo.ASCENDING)
+
+for item in items:
+    print(item.get('boughtitems'))
+
+customers.distinct("firstname")
+
+for i in customers.find({"$or": [{'firstname':'Bob'}, {'firstname':'Amy'}]},
+                                 {'firstname':1, 'boughtitems':1, '_id':0}):
+    print(i)
